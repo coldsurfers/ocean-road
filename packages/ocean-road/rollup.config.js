fe7f3b1c6713b5f65410e9e6bf3be203974c42a0
yungblud <killi8n@gmail.com>
2023-05-01 13:12:34 +0900

    feat: :zap: build setup

---
+++ packages/ocean-road/rollup.config.js
@@ -0,0 +1,37 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const { babel } = require('@rollup/plugin-babel')
+const { nodeResolve } = require('@rollup/plugin-node-resolve')
+const peerDepsExternal = require('rollup-plugin-peer-deps-external')
+const pkg = require('./package.json')
+
+const extensions = ['.js', '.jsx', '.ts', '.tsx']
+
+const config = {
+  input: './src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+    },
+    {
+      file: pkg.module,
+      format: 'esm',
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    nodeResolve({
+      extensions,
+    }),
+    babel({
+      extensions,
+      babelHelpers: 'bundled',
+      presets: [
+        '@babel/preset-typescript',
+        ['@babel/preset-react', { runtime: 'automatic' }],
+      ],
+    }),
+  ],
+}
+
+module.exports = config
