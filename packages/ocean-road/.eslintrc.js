6298419337aed24d4cfe19162f168297b4196edf
yungblud <killi8n@gmail.com>
2023-05-01 13:12:33 +0900

    feat: :zap: modified lint rules

--- packages/ocean-road/.eslintrc.js
+++ packages/ocean-road/.eslintrc.js
@@ -1,3 +1,16 @@
 module.exports = {
-  extends: ["coldsurfers/react-typescript"],
-};
+  extends: ['coldsurfers/react-typescript'],
+  rules: {
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: false,
+        optionalDependencies: false,
+        peerDependencies: true,
+      },
+    ],
+  },
+  env: {
+    browser: true,
+  },
+}
