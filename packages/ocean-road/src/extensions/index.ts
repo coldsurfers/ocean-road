e87435b3e53bf24147fc3e0745e9b8af87d565c0
yungblud <killi8n@gmail.com>
2026-01-16 21:07:50 +0900

    refactor(ocean-road): use compound pattern GridCardList

--- packages/ocean-road/src/extensions/index.ts
+++ packages/ocean-road/src/extensions/index.ts
@@ -1,3 +1,11 @@
-export * from './grid-card-item';
-export * from './grid-card-list-load-more';
-export * from './grid-card-list';
+import { GridCardItem, type GridCardListItemProps } from './grid-card-item';
+import { GridCardList as GridCardListUI } from './grid-card-list';
+import { GridCardListLoadMore } from './grid-card-list-load-more';
+
+export const GridCardList = {
+  List: GridCardListUI,
+  Item: GridCardItem,
+  LoadMore: GridCardListLoadMore,
+};
+
+export type { GridCardListItemProps };
