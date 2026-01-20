1ad2426045482effc851f34498ddb2bb3920be54
yungblud <killi8n@gmail.com>
2026-01-20 23:57:41 +0900

    fix(storybook-app): pnpm monorepo setup

--- apps/StorybookApp/metro.config.js
+++ apps/StorybookApp/metro.config.js
@@ -3,13 +3,48 @@ const withStorybook = require('@storybook/react-native/metro/withStorybook');
 
 const defaultConfig = getDefaultConfig(__dirname);
 
+const path = require('node:path');
+
+const workspaceRoot = path.resolve(__dirname, '../..');
+const projectRoot = __dirname;
+
 /**
  * Metro configuration
  * https://facebook.github.io/metro/docs/configuration
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  watchFolders: [workspaceRoot],
+  transformer: {
+    unstable_allowRequireContext: true,
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+  server: {
+    port: 8081,
+  },
+  resolver: {
+    nodeModulesPaths: [
+      path.resolve(projectRoot, 'node_modules'),
+      path.resolve(workspaceRoot, 'node_modules'),
+    ],
+    resolveRequest: (context, moduleName, platform) => {
+      // https://github.com/pmndrs/zustand/discussions/1967#discussioncomment-13471821
+      // zustand import.meta issue
+      if (moduleName.includes('zustand')) {
+        const result = require.resolve(moduleName); // gets CommonJS version
+        return context.resolveRequest(context, result, platform);
+      }
+      // otherwise chain to the standard Metro resolver.
+      return context.resolveRequest(context, moduleName, platform);
+    },
+  },
+};
 
 const finalConfig = mergeConfig(defaultConfig, config);
 
