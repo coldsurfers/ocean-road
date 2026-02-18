c442c004e6d18d07a42c3fc83edf71432e55ba3d
yungblud <killi8n@gmail.com>
2026-02-18 11:43:15 +0900

    fix(ocean-road): shared list container grid styles

--- packages/ocean-road/src/extensions/grid-card-list/grid-card-list.masonry.styled.tsx
+++ packages/ocean-road/src/extensions/grid-card-list/grid-card-list.masonry.styled.tsx
@@ -1,7 +1,6 @@
 import { Text } from '@/base';
 import { semantics } from '@/tokens';
 import { media } from '@/utils';
-import { commonHorizontalLayoutCss } from '@/utils/common-styles';
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
@@ -30,26 +29,6 @@ export const StyledListHeaderText = styled(Text)`
 
   ${media.medium(css`
     font-size: 1.875rem;
-  `)}
-`;
-
-export const StyledGridContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(4, minmax(0px, 1fr));
-  gap: 1.25rem;
-
-  ${commonHorizontalLayoutCss(['left', 'right'])}
-
-  ${media.large(css`
-    grid-template-columns: repeat(3, minmax(0px, 1fr));
-    gap: 1rem;
-  `)}
-  ${media.medium(css`
-    grid-template-columns: repeat(2, minmax(0px, 1fr));
-    gap: 0.85rem;
-  `)}
-  ${media.small(css`
-    grid-template-columns: repeat(2, minmax(0px, 1fr));
   `)}
 `;
 
--- packages/ocean-road/src/extensions/grid-card-list/grid-card-list.styled.tsx
+++ packages/ocean-road/src/extensions/grid-card-list/grid-card-list.styled.tsx
@@ -1,18 +1,23 @@
 import { Text } from '@/base';
 import { media } from '@/utils';
+import { commonHorizontalLayoutCss } from '@/utils/common-styles';
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
 export const StyledGridContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(4, minmax(0px, 1fr));
-  gap: 1rem;
+  gap: 1.25rem;
+
+  ${commonHorizontalLayoutCss(['left', 'right'])}
 
   ${media.large(css`
     grid-template-columns: repeat(3, minmax(0px, 1fr));
+    gap: 1rem;
   `)}
   ${media.medium(css`
     grid-template-columns: repeat(2, minmax(0px, 1fr));
+    gap: 0.85rem;
   `)}
   ${media.small(css`
     grid-template-columns: repeat(2, minmax(0px, 1fr));
